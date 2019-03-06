32d6cfc77f2c2f5a4bd680e876e08ec803d0cb2a
Jessica Lowry <jdl14a@dmc-207-04.local>
2019-03-06 08:50:58 -0600

    Finished Veggie JSON

--- SimpleJSON_JDL/DataManager.swift
+++ SimpleJSON_JDL/DataManager.swift
@@ -9,30 +9,39 @@
 import UIKit
 
 class DataManager: NSObject {
+    
+    let MYJSONURL = "https://api.myjson.com/bins/136w0u"
+    
+    var dataArray = ["No data yet, pally"]
 
-    func getData()
-    {
-        let urlString = "https://api.myjson.com/bins/136w0u"
-        
-        let actualUrl = URL(string: urlString)
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: actualUrl!) { (data, response, error) in
-            if error != nil
-                //handle error
-            {
+        func getData(completion: @escaping (_ success: Bool) ->())
+        {
+            var success = true
+            let actualUrl = URL(string: MYJSONURL)
+            
+            let task = URLSession.shared.dataTask(with: actualUrl!){
+                (data, response, error) in
                 
-            }
-            
-            if let successfulData = data
-            {
-                print(String(data: successfulData, encoding: String.Encoding.ascii))
-            }
-            
-            
+                if let _ = data, error == nil {
+                    
+                    if let jsonObj = try?
+                        JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
+                    {
+                        print(jsonObj!.value(forKey:"characters")!)
+                        
+                        if let veggieArray = jsonObj!.value(forKey: "characters")as? Array<String>
+                        {
+                            self.dataArray = veggieArray
+                            
+                            print(jsonObj!.value (forKey: "characters")!)
+                        }
+                    }
+                } else {
+                    success = false
+                    }
+                completion(success)
         }
-        task.resume()
+         task.resume()
+        }
         
     }
-}
